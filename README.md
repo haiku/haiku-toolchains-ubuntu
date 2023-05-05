5b5d2e031b1991b663f4db8669e1f42d5418e55d
Trung Nguyen <57174311+trungnt2910@users.noreply.github.com>
2023-05-05 22:32:10 +1000

    feat: Add fetch.sh
    This script can be shared by the two CI scripts as well as third
    party consumers of this repo.

--- README.md
+++ README.md
@@ -1,3 +1,37 @@
 # haiku-tools
 
 This repository contains pre-built Haiku cross-compilation tools for x86_64 Ubuntu Linux.
+
+## Host tools
+
+Host tools are tools that are meant to be run on the host device building Haiku and/or cross-compiling
+apps for Haiku. They are targets that have the `<build>` prefix in the Haiku build system.
+
+These tools are built either at the start of a month or manually by a maintainer when there is a
+major change.
+
+## Build tools
+
+Build tools are the cross-compiler and other tools required to cross-compile applications for Haiku.
+Build tools are specific for each Haiku architecture, but currently only the tools for `x86_64` are
+built.
+
+This repo checks for any updates from the [buildtools](https://github.com/haiku/buildtools) repo
+once a day. If any new revisions are detected, it will perform a build.
+
+## Obtaining
+
+A script, `fetch.sh`, is included in this repo. This script will fetch the latest release of
+either the host tools or the build tools.
+
+To fetch the host tools:
+
+```sh
+curl -sLOJ $(./fetch.sh --hosttools)
+```
+
+To fetch the build tools:
+
+```sh
+curl -sLOJ $(./fetch.sh --buildtools --arch=x86_64)
+```
---
+++ fetch.sh
@@ -0,0 +1,79 @@
+#!/bin/bash -e
+
+GITHUB_REPOSITORY="trungnt2910/haiku-tools"
+HAIKU_ARCH="x86_64"
+
+for opt in "$@"
+do
+    case $opt in
+        -b|--buildtools)
+            PACKAGE_NAME="buildtools"
+            PACKAGE_REVISION="btrev"
+            shift
+            ;;
+        -h|--hosttools)
+            PACKAGE_NAME="hosttools"
+            PACKAGE_REVISION="hrev"
+            shift
+            ;;
+        -a=*|--arch=*)
+            HAIKU_ARCH="${opt#*=}"
+            shift
+            ;;
+        -r=*|--repository=*)
+            GITHUB_REPOSITORY="${opt#*=}"
+            shift
+            ;;
+        --rev-only)
+            REV_ONLY=1
+            shift
+            ;;
+        *)
+            echo "Unknown option: $opt"
+            exit 1
+    esac
+done
+
+if [ -z "$PACKAGE_NAME" ]; then
+    echo "Error: No package name specified."
+    exit 1
+fi
+
+# Only buildtools are architecture-specific
+if [ "$PACKAGE_NAME" = "buildtools" ]; then
+    HAIKU_ARCH_SUFFIX="$HAIKU_ARCH-"
+fi
+
+latestRev=""
+releaseUrl="https://github.com/$GITHUB_REPOSITORY/releases/tag/$PACKAGE_NAME-$HAIKU_ARCH_SUFFIX"
+pageNum=1
+while [ -z "$latestRev" ];
+do
+    # Fetch using GitHub API
+    json=$(curl -s https://api.github.com/repos/$GITHUB_REPOSITORY/releases?page=$pageNum)
+    pageNum=$((pageNum + 1))
+    if [ $(echo $json | jq length) -eq 0 ]; then
+        # This means that we've passed the end and reached an empty array
+        break
+    fi
+    # Store array of revisions
+    revisions=($(echo $json | jq -e -r ".[] | .html_url | select(contains(\"$PACKAGE_NAME\"))[${#releaseUrl}:]")) \
+        || continue
+    latestRev=${revisions[0]}
+done
+
+if [ -z "$latestRev" ]; then
+    echo "Error: No revision found."
+    exit 1
+fi
+
+if [ -n "$REV_ONLY" ]; then
+    echo $latestRev
+    exit 0
+fi
+
+hostArch=$(uname -m)
+hostOs=$(uname -s | tr '[:upper:]' '[:lower:]')
+releaseName="$PACKAGE_NAME-$HAIKU_ARCH_SUFFIX$latestRev"
+echo "https://github.com/$GITHUB_REPOSITORY/releases/download/$releaseName/$hostArch-$hostOs-$releaseName.zip"
+exit 0
